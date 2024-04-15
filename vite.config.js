7f13b83d45becc843cb79aa18f9ff1d482436f2f
Cristian Perrone <cristian.perrone@dedalord.com>
2024-04-14 22:45:05 -0300

    add github pages support

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://3dviewer.cristianperrone.com',
   plugins: [react()],
   resolve: {
     alias: {
